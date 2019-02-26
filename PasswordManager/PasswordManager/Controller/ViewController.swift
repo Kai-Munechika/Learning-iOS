c0501b10f5aea5c7ede7579041835cb7a0582f3d
Kai Munechika <kaim808@gmail.com>
2019-02-25 18:37:04 -1000

    PasswordManager: implemeted adding, editing, and deleting account items

--- PasswordManager/PasswordManager/Controller/ViewController.swift
+++ PasswordManager/PasswordManager/Controller/ViewController.swift
@@ -23,23 +23,35 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.rowHeight = 68
         
-//        var accountItem = AccountItem(context: context)
-//        accountItem.appName = "M1 Finance"
-//        accountItem.email = "kaim808@gmail.com" 
-//        accountItem.password = "testpassword123"
-//                
-//        accountItem = AccountItem(context: context)
-//        accountItem.appName = "Robinhood"
-//        accountItem.email = "kaim808@gmail.com" 
-//        accountItem.password = "testpassword123"
-//        saveItems()
-        
         loadItems()
     }
     
     @IBAction func addClicked(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add account", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { $0.placeholder = "App name" }
+        alert.addTextField { $0.placeholder = "Email" }
+        alert.addTextField { $0.placeholder = "Password"; $0.isSecureTextEntry = true }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel)) 
+        alert.addAction(UIAlertAction(title: "Add", style: .default) { [unowned self] (action) in
+            let item = AccountItem(context: self.context)
+            item.appName = alert.textFields?[0].text
+            item.email = alert.textFields?[1].text
+            item.password = alert.textFields?[2].text
+            
+            self.items.append(item)
+            self.saveItems()
+            
+            let indexPath = IndexPath(row: self.items.count - 1, section: 0)
+            self.tableView.insertRows(at: [indexPath], with: .automatic)
+        })
+        
+        present(alert, animated: true, completion: nil)
     }
     
+    // MARK: - Data Persistence Methods
     func saveItems() {
         do {
             try context.save()
@@ -61,7 +73,7 @@ class ViewController: UIViewController {
 }
 
 
-// MARK: - UITableViewDataSource methods
+// MARK: - UITableViewDataSource Methods
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
@@ -76,11 +88,43 @@ extension ViewController: UITableViewDataSource {
     }
 }
 
-// MARK: - UITableViewDelegate methods
+// MARK: - UITableViewDelegate Methods
 extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt: IndexPath) -> [UITableViewRowAction]? {
+        
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") { [unowned self] action, index in
+            self.context.delete(self.items[index.row])
+            self.saveItems()
+            
+            self.items.remove(at: index.row)
+            self.tableView.deleteRows(at: [index], with: .automatic)
+        }
+        
+        let editAction = UITableViewRowAction(style: .normal, title: "Edit") { [unowned self] action, index in
+            let selectedItem = self.items[index.row]
+            let alert = UIAlertController(title: "Edit account", message: "", preferredStyle: .alert)
+            
+            alert.addTextField { $0.placeholder = "App name"; $0.text =  selectedItem.appName }
+            alert.addTextField { $0.placeholder = "Email"; $0.text = selectedItem.email }
+            alert.addTextField { $0.placeholder = "Password"; $0.isSecureTextEntry = true; $0.text = selectedItem.password }
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel)) 
+            alert.addAction(UIAlertAction(title: "Save", style: .default) {(action) in
+                selectedItem.appName = alert.textFields?[0].text
+                selectedItem.email = alert.textFields?[1].text
+                selectedItem.password = alert.textFields?[2].text
+                
+                self.saveItems()
+                self.tableView.reloadRows(at: [index], with: .automatic)
+            })   
+            self.present(alert, animated: true, completion: nil)
+        }
+        return [deleteAction, editAction]
     }
 }
 
